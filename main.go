7a4267859e2d62f8627df7a68e8f7bb5524aeecc
luanorlando <orlandoluan@hotmail.com>
2026-02-16 18:16:20 -0300

    feat: add package server

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
+	"server"
 	"strings"
 	"time"
 )
@@ -13,91 +13,11 @@ type apiResult struct {
 	Body   string
 }
 
-func requestBrasilCep(cep string, ch chan<- apiResult) {
-	url := "https://brasilapi.com.br/api/cep/v1/" + cep
-	req, err := http.NewRequest("GET", url, nil)
-
-	emptyResult := apiResult{
-		Source: "",
-		Body:   "",
-	}
-
-	if err != nil {
-		ch <- emptyResult
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		ch <- emptyResult
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		ch <- apiResult{
-			Source: "Brasil CEP",
-			Body:   fmt.Sprintf("Erro: status %d recebido da API", resp.StatusCode),
-		}
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		ch <- emptyResult
-	}
-
-	ch <- apiResult{
-		Source: "Brasil CEP",
-		Body:   string(body),
-	}
-}
-
-func requestViaCep(cep string, ch chan<- apiResult) {
-	url := "http://viacep.com.br/ws/" + cep + "/json/"
-
-	emptyResult := apiResult{
-		Source: "",
-		Body:   "",
-	}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		ch <- emptyResult
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		ch <- emptyResult
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		ch <- apiResult{
-			Source: "Via CEP",
-			Body:   fmt.Sprintf("Erro: status %d recebido da API", resp.StatusCode),
-		}
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		ch <- emptyResult
-	}
-
-	ch <- apiResult{
-		Source: "Via CEP",
-		Body:   string(body),
-	}
-}
-
 func handleConcuerncy(cep string) {
 	ch := make(chan apiResult)
 
-	go requestBrasilCep(cep, ch)
-	go requestViaCep(cep, ch)
+	go server.RequestBrasilCep(cep, ch)
+	go server.RequestViaCep(cep, ch)
 
 	select {
 	case result := <-ch:
